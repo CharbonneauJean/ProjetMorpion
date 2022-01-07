e7e532e0ca7a97cacfbea78c9e7b67c8c8b39831
Jean Charbonneau <jcharbonneau.ext@simplonformations.onmicrosoft.com>
2022-01-07 08:59:51 +0100

    Refactor for names

--- Morpion.py
+++ Morpion.py
@@ -1,79 +1,79 @@
 class Morpion:
 
-    joueur = 1
-    grille = []
+    currentPlayer = 1
+    grid = []
 
     def __init__(self):
         joueur = 1
-        self.grille=[" "," "," "," "," "," "," "," "," "]
+        self.grid=[" "," "," "," "," "," "," "," "," "]
 
     def showGrid(self):
         print("     0   1   2 ")
         print("   -------------")
         print("0 ", end='')
         for i in range(3):
-            print(" | "+str(self.grille[i]), end='')
+            print(" | "+str(self.grid[i]), end='')
         print(" |")
         print("   -------------")
         print("1 ", end='')
         for i in range(3):
-            print(" | "+str(self.grille[i+3]), end='')
+            print(" | "+str(self.grid[i+3]), end='')
         print(" |")
         print("   -------------")
         print("2 ", end='')
         for i in range(3):
-            print(" | "+str(self.grille[i+6]), end='')
+            print(" | "+str(self.grid[i+6]), end='')
         print(" |")
         print("   -------------")
 
 
     def newTurn(self):
-        print("Tour du joueur "+str(self.joueur))
-        colonne=input("Colonne : ")
-        ligne=input("Ligne : ")
-        print("Vous avez coché la case ("+colonne+","+ligne+")")
-        while self.grille[int(colonne)+int(ligne)*3]!=" ":
+        print("Tour du joueur "+str(self.currentPlayer))
+        column=input("Colonne : ")
+        line=input("Ligne : ")
+        print("Vous avez coché la case ("+column+","+line+")")
+        while self.grid[int(column)+int(line)*3]!=" ":
             self.showGrid()
             print("Cette case est deja cochée ! Veuillez en choisir une autre.")
-            colonne=input("Colonne : ")
-            ligne=input("Ligne : ")
-            print("Vous avez coché la case ("+colonne+","+ligne+")")
+            column=input("Colonne : ")
+            line=input("Ligne : ")
+            print("Vous avez coché la case ("+column+","+line+")")
 
-        if self.joueur==1 :
-            self.grille[int(colonne)+int(ligne)*3]="X"
-        if self.joueur==2 :
-            self.grille[int(colonne)+int(ligne)*3]="O"
+        if self.currentPlayer==1 :
+            self.grid[int(column)+int(line)*3]="X"
+        if self.currentPlayer==2 :
+            self.grid[int(column)+int(line)*3]="O"
         self.showGrid()
 
     def hasWinner(self):
-        if (self.grille[0]==self.grille[1]) and (self.grille[0]==self.grille[2]) and (self.grille[0]!=" "):
+        if (self.grid[0]==self.grid[1]) and (self.grid[0]==self.grid[2]) and (self.grid[0]!=" "):
             return 1
-        if (self.grille[3]==self.grille[4]) and (self.grille[3]==self.grille[5]) and (self.grille[3]!=" "):
+        if (self.grid[3]==self.grid[4]) and (self.grid[3]==self.grid[5]) and (self.grid[3]!=" "):
             return 1
-        if (self.grille[6]==self.grille[7]) and (self.grille[6]==self.grille[8]) and (self.grille[6]!=" "):
+        if (self.grid[6]==self.grid[7]) and (self.grid[6]==self.grid[8]) and (self.grid[6]!=" "):
             return 1
-        if (self.grille[0]==self.grille[3]) and (self.grille[0]==self.grille[6]) and (self.grille[0]!=" "):
+        if (self.grid[0]==self.grid[3]) and (self.grid[0]==self.grid[6]) and (self.grid[0]!=" "):
             return 1
-        if (self.grille[1]==self.grille[4]) and (self.grille[1]==self.grille[7]) and (self.grille[1]!=" "):
+        if (self.grid[1]==self.grid[4]) and (self.grid[1]==self.grid[7]) and (self.grid[1]!=" "):
             return 1
-        if (self.grille[2]==self.grille[5]) and (self.grille[2]==self.grille[8]) and (self.grille[2]!=" "):
+        if (self.grid[2]==self.grid[5]) and (self.grid[2]==self.grid[8]) and (self.grid[2]!=" "):
             return 1
-        if (self.grille[0]==self.grille[4]) and (self.grille[0]==self.grille[8]) and (self.grille[0]!=" "):
+        if (self.grid[0]==self.grid[4]) and (self.grid[0]==self.grid[8]) and (self.grid[0]!=" "):
             return 1
-        if (self.grille[2]==self.grille[4]) and (self.grille[2]==self.grille[6]) and (self.grille[2]!=" "):
+        if (self.grid[2]==self.grid[4]) and (self.grid[2]==self.grid[6]) and (self.grid[2]!=" "):
             return 1
 
 
     def isDraw(self):
         for i in range(9):
-            if self.grille[i]==" ":
+            if self.grid[i]==" ":
                 return 0
         return 1
 
 
 jeuMorpion = Morpion()
 
-jeuMorpion.joueur=1
+jeuMorpion.currentPlayer=1
 print("Le joueur 1 joue avec les X et le joueur 2 avec les O")
 
 jeuMorpion.showGrid()
@@ -81,13 +81,13 @@ gagne=0
 while gagne==0:
     jeuMorpion.newTurn()
     if jeuMorpion.hasWinner():
-        print("Le joueur "+str(jeuMorpion.joueur)+" a gagné !")
+        print("Le joueur "+str(jeuMorpion.currentPlayer)+" a gagné !")
         gagne=1
     else:
         if jeuMorpion.isDraw():
             print("Match nul !")
             gagne=1
-    if jeuMorpion.joueur==1:
-        jeuMorpion.joueur=2
+    if jeuMorpion.currentPlayer==1:
+        jeuMorpion.currentPlayer=2
     else:
-        jeuMorpion.joueur=1
+        jeuMorpion.currentPlayer=1
